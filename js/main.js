30876a5b835306acde93df3d14812ae2be6d3912
Martins Martinsons <martins.martinsons@yahoo.com>
2020-11-19 07:39:11 +0200

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+const dayOfYear = document.querySelector('#day-of-year');
+const dayOfWeek = document.querySelector('#day-of-week');
+const weekNumber = document.querySelector('#week-number');
 
+
+let day = new Date();
 
 
 // TIMEZONE
