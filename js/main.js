413d0ef51461546e53c6296cc834aff6dc3b50d8
Martins Martinsons <martins.martinsons@yahoo.com>
2020-11-30 10:12:28 +0200

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const dayOfYear = document.querySelector('#day-of-year');
 const dayOfWeek = document.querySelector('#day-of-week');
 const weekNumber = document.querySelector('#week-number');
+const clockBtn = document.querySelector('#clock_btn');
+const infoSection = document.querySelector('#info');
+const quotesSection = document.querySelector('#quotes');
 
 
 let day = new Date();
@@ -16,4 +19,6 @@ paragr.innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
 timeZone.appendChild(paragr);
 
 
-
+clockBtn.addEventListener('click', function(){
+    console.log('Hello');
+})
