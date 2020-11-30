5711987760cb32c139f1727f29c05b378623153f
Martins Martinsons <martins.martinsons@yahoo.com>
2020-11-30 09:33:21 +0200

    initial commit

--- js/clock.js
+++ js/clock.js
@@ -106,6 +106,20 @@ function weekNr() {
 weekNr();
 
 
+function dayYear() {
+    let now = new Date();
+    let start = new Date(now.getFullYear(), 0, 0);
+    let diff = now - start;
+    let oneDay = 1000 * 60 * 60 * 24;
+    let day = Math.floor(diff / oneDay);
+    
+    const para = document.createElement('p');
 
+    para.innerHTML = day;
+
+    dayOfYear.appendChild(para);
+}
+
+dayYear();
 
   
